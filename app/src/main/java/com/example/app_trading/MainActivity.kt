c7f55aca5e6906407f829bfd35a67b41aadac4dd
azuberogon <azuberogondam1b@gmail.com>
2025-01-23 22:53:00 +0100

    test api
    -se habilito el acceso a internet
    -se añadió funciones al main para conectarse con el servidor
    -no da ningún fallo

--- app/src/main/java/com/example/app_trading/MainActivity.kt
+++ app/src/main/java/com/example/app_trading/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.app_trading
 
 import android.os.Bundle
+import android.widget.TextView
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
     /*override fun onCreate(savedInstanceState: Bundle?) { // se sobre escribe la actividad actual
@@ -17,6 +21,40 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }*/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView) // Asegúrate de tener un TextView en tu XML
+        fetchDataFromApi(textView)
+    }
+
+    fun fetchDataFromApi(textView: TextView) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://api.tiingo.com/api/test?token=15fd6a8cb82c76c6e845ef46f47956c4319ecaac")
+            .addHeader("Content-Type", "application/json")
+            .build()
+
+        Thread {
+            try {
+                val response = client.newCall(request).execute()
+                if (response.isSuccessful) {
+                    val responseData = response.body?.string()
+                    runOnUiThread {
+                        textView.text = responseData // Actualiza el texto en el hilo principal
+                    }
+                } else {
+                    runOnUiThread {
+                        textView.text = "Error: ${response.code}"
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
 
 
 }
